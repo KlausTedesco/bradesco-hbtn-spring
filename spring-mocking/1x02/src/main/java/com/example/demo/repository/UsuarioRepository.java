423083f95ab2343b07274c6a46f90f66f0e26cfb
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-12-07 17:45:46 -0300

    Ajuste usuario repository

--- spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository {
+
+	void save(Usuario entity);
+
+	Optional<Usuario> findById(Long id);
 	
 }
